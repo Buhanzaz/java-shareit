665b71413d0ec84d205b4bf365cd9c08d8abcd54
Eugene Bukhanov <buhanza21@yandex.ru>
2023-10-24 22:05:49 +0300

    feat: Добавлены Item и ItemDto

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Item {
+    Long id; //Id вещи.
+    String name; //Название вещи.
+    String description; //Описание.
+    Boolean available; //Статус доступности вещи True - доступно, False - нет.
+    Long userId; //Владелец вещи ownerId == userId.
+    @JsonIgnore
+    Boolean isRequest; //True - вещь создана другим пользователем, False - владельцем вещи.
+    Set<Long> reviews; //Собраны id отзывов.
 }
