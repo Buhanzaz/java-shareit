7a3da942114244eb8fd5904b88b31b5259383dc3
Buhanzaz <Buhanza@gmail.com>
2023-11-06 04:43:17 +0300

    feat: Добавил реализацию сервисного слоя работающего с db
    feat: Добавил реализацию сервисного слоя работающего с db

---
+++ src/main/java/ru/practicum/shareit/item/service/db/ItemServiceImplInDB.java
@@ -0,0 +1,64 @@
+package ru.practicum.shareit.item.service.db;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidateException;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repository.db.ItemRepositoryInDB;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.db.UserRepositoryInDB;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+class ItemServiceImplInDB implements ItemService {
+
+    ItemRepositoryInDB itemRepository;
+    UserRepositoryInDB userRepository;
+    ItemMapper itemMapper;
+
+    @Override
+    public ItemDto addItem(Long userId, ItemDto dto) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("Юзер с id " + userId + " не найден"));
+        dto.setUser(user);
+        return itemMapper.toDto(itemRepository.save(itemMapper.toModel(dto)));
+    }
+
+    @Override
+    public ItemDto updateItem(Long itemId, Long userId, ItemDto dto) {
+        Item item = itemRepository.findItemByUserIdAndItemId(userId, itemId).orElseThrow(() -> new NotFoundException("Предмет с id " + itemId + " не найден"));
+        itemMapper.updateItem(item, dto);
+        itemRepository.save(item);
+
+        return itemMapper.toDto(item);
+    }
+
+    @Override
+    public ItemDto getItemById(Long itemId, Long userid) {
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("Предмет с id " + itemId + " не найден"));
+        return itemMapper.toDto(item);
+    }
+
+    @Override
+    public List<ItemDto> getAllItemsOwner(Long userId) {
+        return itemRepository.findAll().stream()
+                .filter(item -> item.getUser().getId() == userId)
+                .map(itemMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ItemDto> itemSearch(String itemName, Long userid) {
+        return null;
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/user/service/db/UserServiceImplInDB.java
@@ -0,0 +1,56 @@
+package ru.practicum.shareit.user.service.db;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.db.UserRepositoryInDB;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+class UserServiceImplInDB implements UserService {
+
+    UserRepositoryInDB userRepository;
+    UserMapper userMapper;
+
+    @Override
+    public UserDto addUser(UserDto dto) {
+        return userMapper.toDto(userRepository.save(userMapper.toModel(dto)));
+    }
+
+    @Override
+    public UserDto updateUser(Long userId, UserDto dto) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("Юзер с id " + userId + " не найден")
+        );
+        return userMapper.toDto(userRepository.save(userMapper.updateUser(user, dto)));
+    }
+
+    @Override
+    public UserDto getUserById(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new NotFoundException("Юзер с id " + userId + " не найден")
+        );
+
+        return userMapper.toDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream().map(userMapper::toDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
