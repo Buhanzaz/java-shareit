ec0b0153a78dc8f68fc6a4b53c46859b55e33fd6
Eugene Bukhanov <buhanza21@yandex.ru>
2023-11-11 23:05:05 +0300

    feat: Добавил новые классы для вылавливания ошибок

---
+++ src/main/java/ru/practicum/shareit/exception/DataTimeException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class DataTimeException extends RuntimeException {
+    public DataTimeException(String message) {
+        super(message);
+    }
+}
