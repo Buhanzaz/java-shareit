afb095a40ecb470a5c30995e99f1bed5a6f48f97
Eugene Bukhanov <buhanza21@yandex.ru>
2023-11-25 23:36:54 +0300

    refactor: Добавил новые аргументы в сигнатуры методов и реализацию пагинации в репозиторий

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,11 +1,14 @@
 package ru.practicum.shareit.request.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 
-    List<ItemRequest> searchItemRequestByCreator_IdOrderByCreatedAsc(Long creatorId);
+    List<ItemRequest> searchItemRequestByCreator_IdOrderByCreatedAsc(Long creatorId, Pageable page);
+    Optional<ItemRequest> searchItemRequestByIdOrderByCreatedAsc(Long creatorId);
 }
